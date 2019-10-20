8ac7f900ff7439985f6c36a4b1339ae5cb45e164
magic8baller <lturis@gmail.com>
2019-10-20 10:56:57 -0400

    createPost query works locally!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-		<div>Γεια σου κόσμε</div>
-  );
+import CreatePost from './createPost';
+function App () {
+	return (
+		<div>
+			<h1>Γεια σου κόσμε</h1>
+			<CreatePost />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/createPost.js
@@ -0,0 +1,63 @@
+import gql from "graphql-tag";
+import React from "react";
+import {Mutation} from "react-apollo";
+import {createPost} from "./graphql/mutations";
+
+class CreatePost extends React.Component {
+	handleSubmit = (e, createPost) => {
+		e.preventDefault();
+		createPost({
+			variables: {
+				input: {
+					title: this.title.value,
+					body: this.body.value,
+					createdAt: new Date().toISOString()
+				}
+			}
+		}).then(res => {
+			this.title.value = "";
+			this.body.value = "";
+		});
+	};
+	render () {
+		return (
+			<div style={{textAlign: "center"}}>
+				<h1>Create post</h1>
+
+				<Mutation mutation={gql(createPost)}>
+					{(createPost, {data, loading, error}) => {
+						return (
+							<div>
+								<form
+									className="add-post"
+									onSubmit={e => this.handleSubmit(e, createPost)}
+								>
+									<div style={{paddingBottom: '1rem'}}>
+										<input type="text" placeholder="Title"
+											ref={node => (this.title = node)}
+											required
+										/>
+									</div>
+									<div style={{paddingBottom: '1rem'}}>
+										<textarea
+											rows="10"
+											cols="40"
+											placeholder="Body"
+											ref={node => (this.body = node)}
+											required
+										/>
+									</div>
+									<button>{loading ? "Yes boss..." : "Create Post"}
+									</button>
+								</form>
+								{error && <p>{error.message}</p>}
+							</div>
+						);
+					}}
+				</Mutation>
+			</div>
+		);
+	}
+}
+
+export default CreatePost;
